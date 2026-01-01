3c34c9e98d5e5fad73396d2aafeb5a1f2e0b395e
Sara Vishi <vishisara@gmail.com>
2026-01-01 20:45:09 +0100

    final react

--- products-page/src/main.jsx
+++ products-page/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Importimi i Bootstrap (UI Library) - kjo plotëson 20% të kërkesës
+// 1. Bootstrap i pari
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Importimi i stileve globale
-import './index.css';
+// 2. Stili yt i fundit (për t'i mbishkruar ngjyrat)
+import './App.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
